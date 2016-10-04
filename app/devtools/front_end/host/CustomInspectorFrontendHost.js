72356e8f7bab43f04236a0878e9d22848f11bff0
Connor Pearson <cjp822@gmail.com>
2016-10-04 15:34:20 -0400

    update devtools

--- app/devtools/front_end/host/CustomInspectorFrontendHost.js
+++
@@ -1,1 +0,0 @@
-../../../devtools_overrides/CustomInspectorFrontendHost.js
--- scripts/build_generated_scripts.js
+++ scripts/build_generated_scripts.js
@@ -1,19 +1,24 @@
 /* jshint evil:true */
 /* global InspectorBackendClass */
 var fs = require('fs'),
-    protocol = require(__dirname + '/../app/devtools/protocol.json');
+    protocol = require(__dirname + '/../protocol.json');
 
 // Mock objects
 Promise = require("es6-promise").Promise;
 self = this;
 window = {};
 WebInspector = {};
+InspectorBackend = {
+    isInitialized: function () {
+	return true;
+    }
+};
 
 eval(fs.readFileSync(__dirname + '/../app/devtools/front_end/platform/utilities.js', 'utf8'));
 eval(fs.readFileSync(__dirname + '/../app/devtools/front_end/common/Object.js', 'utf8'));
-eval(fs.readFileSync(__dirname + '/../app/devtools/front_end/sdk/InspectorBackend.js', 'utf8'));
+eval(fs.readFileSync(__dirname + '/../app/devtools/front_end/sdk/InspectorBackendHostedMode.js', 'utf8'));
 
-var commands = InspectorBackendClass._generateCommands(protocol);
+var commands = WebInspector.InspectorBackendHostedMode.generateCommands(protocol);
 var header = '// Auto-generated.\n' +
              '// Run `node --harmony scripts/generate-commands.js` to update.\n' +
              '\n';
@@ -21,5 +26,5 @@ var header = '// Auto-generated.\n' +
 fs.writeFileSync(__dirname + '/../app/devtools/front_end/InspectorBackendCommands.js', header + commands);
 
 // Generate empty SupportedCSSProperties.js for now.
-var supportedCSSPropertiesContent = 'WebInspector.CSSMetadata.initializeWithSupportedProperties({});'
+var supportedCSSPropertiesContent = 'WebInspector.CSSMetadata._generatedProperties = []';
 fs.writeFileSync(__dirname + '/../app/devtools/front_end/SupportedCSSProperties.js', supportedCSSPropertiesContent);
